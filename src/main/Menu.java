661edaff022876411597f1729b25a896f28b1777
TazakiN <tazkia.nizami@gmail.com>
2023-09-17 23:41:21 +0700

    init: Struktur Menu Utama
    Inisiasi gambaran perkiraan apa yang ditampilkan saat program dijalankan yaitu perkenalan program dan menu utama

---
+++ src/main/Menu.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Menu {
+    protected static void Start() {
+        System.out.println("Selamat datang di TUBES ALGEO 1");
+        System.out.println("by: ucup tampan");
+        System.out.println("Anggota:");
+        System.out.println("1. (13522015) Yusuf Ardian Sandi ");
+        System.out.println("2. (13522032) Tazkia Nizami");
+        System.out.println("3. (13522057) Moh Fairuz Alauddin Yahya");
+        System.out.println("===================================================================");
+    }
+
+    protected static void Pilihan() {
+        System.out.println("MENU");
+        System.out.println("1. Sistem Persamaan Linier");
+        System.out.println("2. Determinan");
+        System.out.println("3. Matriks Balikan");
+        System.out.println("4. Interpolasi Polinom");
+        System.out.println("5. Interpolasi Bicubic Spline");
+        System.out.println("6. Regresi Linier Balikan");
+        System.out.println("7. Keluar");
+        System.out.println("Katakan Pilihanmu:");
+    }
+}
