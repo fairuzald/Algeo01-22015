96e8698b0f81256628c3d38c0026ed357dd5f8f0
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-09-18 11:35:35 +0700

    ci: Add sprecific error on bat and github CI

--- Run.bat
+++ Run.bat
@@ -11,8 +11,14 @@ if not exist %BIN_DIR% mkdir %BIN_DIR%
 javac -d %BIN_DIR% %TOOLS_DIR%\*.java
 javac -d %BIN_DIR% %SRC_DIR%\*.java
 
+:: Periksa exit code dari Java program
+if %ERRORLEVEL% NEQ 0 (
+  echo Error: Java program exited with an error code.
+  exit /b %ERRORLEVEL%
+)
 :: Jalankan aplikasi Anda dari folder bin
 java -cp %CLASSPATH% Main
+
 
 :: Tunggu sebentar sebelum menutup jendela
 pause
