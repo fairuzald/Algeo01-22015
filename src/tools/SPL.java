97fc950be71528fe7fe652ea1839af2e0f28c0a8
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2023-09-24 10:33:36 +0700

    feat: Separating interface files from program code to make it easier to define dictionary specifications

--- src/tools/SPL.java
+++ src/tools/SPL.java
@@ -3,41 +3,6 @@ package tools;
 import java.io.IOException;
 import java.io.FileWriter;
 
-interface SPLInterface {
-  public void readSPL();
-
-  public void readFileSPL(String fileName);
-
-  public void displaySPL();
-
-  public void writeFileSPL(String fileName);
-
-  public categorySolution solutionType();
-
-  public boolean isAllCoefMatrixDiagonalOne();
-
-  public boolean isAllRowCoefMatrixZero(int row);
-
-  public boolean isRowVectorResultZero(int row);
-
-  public void gaussMethodSPL();
-
-  public void gaussSolution();
-
-  public void gJordanMethodSPL();
-
-  public void inversMethodSPL();
-
-  public void cramerMethodSPL();
-
-
-  public enum categorySolution {
-    PARAMETRIX, UNIQUE, UNDEFINED, SUBSTITABLE
-  }
-
-}
-
-
 public class SPL extends Matrix implements SPLInterface {
   /* ***** ATRIBUTE ***** */
   public float[] Solution;
