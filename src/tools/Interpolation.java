e44c62628cf970b2412d298a744e7f14db02b3e6
Yusufarsan <sandiyusufardian@gmail.com>
2023-09-29 00:53:11 +0700

    Add cetakOutputInterpolasi, cariTaksiranY, and fix readFileMatrixInterpolasi

--- src/tools/Interpolation.java
+++ src/tools/Interpolation.java
@@ -1,5 +1,7 @@
 package tools;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Interpolation {
@@ -37,17 +39,72 @@ public class Interpolation {
         input.close();
         this.koordinat = matrixKoordinat;
         this.n = nInput;
-
+        
     }
-
+    
     // Method untuk membaca masukkan user dari file
-    public void readFileMatrixInterpolasi(int n, float[] xlist, float[] ylist, double x) { // Pahamin cara baca file buat dijadiin input
-        Matrix matrixKoordinat = new Matrix(n, n);
-        matrixKoordinat.readFileMatrix("matrix1.txt");      // ini harusnya sesuai sama masukkan user mau filename nya apa
-        this.koordinat = matrixKoordinat;
-        this.n = matrixKoordinat.getRowEff()-1;
-        // this.xDitaksir =        // Ini masih bingung masukkan nya drmn, nunggu jawaban QnA
+    public void readFileMatrixInterpolasi(final String filePath) {
+    try {
+      /* KAMUS */
+
+      File file = new File(filePath);
+      int Nrow = 0;
+      int Ncol = 0;
+      int i, j; // Indeks
+
+      Scanner matrix = new Scanner(file); // Untuk ambil matrix
+
+      /* ALGORITMA */
+
+      // Menghitung total baris matrix
+      while (matrix.hasNextLine()) {
+        Nrow++;
+        matrix.nextLine();
+      }
+      Nrow --;
+      matrix.close();
+
+      matrix = new Scanner(file);
+      Scanner line = new Scanner(matrix.nextLine());
+      // Menghitung total kolom baris matrix
+      while (line.hasNextDouble()) {
+        Ncol++;
+        line.nextDouble();
+      }
+      line.close();
+      matrix.close();
+      // Reopen the file for reading
+      matrix = new Scanner(file);
+
+      // Inisialisasi matrix temp
+      Matrix matrixTemp = new Matrix(Nrow, Ncol);
+
+      // Read and populate the matrix
+      for (i = 0; i < Nrow; i++) {
+        for (j = 0; j < Ncol; j++) {
+          if (matrix.hasNextDouble()) {
+            matrixTemp.setElmt(i, j, matrix.nextDouble());
+          }
+        }
+      }
+      this.xDitaksir = matrix.nextDouble();
+      matrix.close();
+
+      // Update the matrix and dimensions
+      this.koordinat = matrixTemp;
+    } catch (FileNotFoundException e) {
+      System.err.printf("Error: File \"%s\" tidak ditemukan\n", filePath);
     }
+  }
+
+// INI SALAH
+    // public void readFileMatrixInterpolasi(int n, float[] xlist, float[] ylist, double x) { // Pahamin cara baca file buat dijadiin input
+    //     Matrix matrixKoordinat = new Matrix(n, n);
+    //     matrixKoordinat.readFileMatrix("matrix1.txt");      // ini harusnya sesuai sama masukkan user mau filename nya apa
+    //     this.koordinat = matrixKoordinat;
+    //     this.n = matrixKoordinat.getRowEff()-1;
+    //     // this.xDitaksir =        // Ini masih bingung masukkan nya drmn, nunggu jawaban QnA
+    // }
 
     // Method untuk membuat matrix SPL
     public void buatMatrixSPL()
@@ -83,14 +140,27 @@ public class Interpolation {
         double hasil=0;
         for (int i=0; i<this.matrixSPL.getColEff()-1; i++)
         {
-            hasil += this.matrixSPL.Solution[i];
-            System.out.printf("a%d = %f\n", i, this.matrixSPL.Solution[i]);
+            hasil += this.matrixSPL.Solution[i]*Math.pow(this.xDitaksir, i);
+            // System.out.printf("a%d = %f\n", i, this.matrixSPL.Solution[i]);
         }
         this.hasilTaksiranX = hasil;
     }
 
     // Method untuk nyetak hasil taksiran
-    public void cetakHasilTaksiran() {
-        System.out.printf("Hasil taksiran dari x = %f adalah %f\n", this.xDitaksir, this.hasilTaksiranX);
+    public void cetakOutputInterpolasi() {
+        String output = "f(x) = ";
+        String tempstr;
+        for (int i=this.matrixSPL.getColEff()-1; i>=0; i--)
+        {
+            tempstr = Double.toString(this.matrixSPL.Solution[i]);
+            if (i==0)
+            {
+                output = output + tempstr + ", f("+ this.xDitaksir +") = ";                
+            } else{
+                output = output + tempstr + "x^("+i+") + ";
+            }
+        }
+        output = output + this.hasilTaksiranX;
+        System.out.println(output);
     }
 }
