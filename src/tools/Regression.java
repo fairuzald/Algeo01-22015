a535e462a80d22056c8c78091127b6df03b90b5c
TazakiN <tazkia.nizami@gmail.com>
2023-09-28 18:04:26 +0700

    Adding function to display output of Regression

--- src/tools/Regression.java
+++ src/tools/Regression.java
@@ -1,13 +1,14 @@
 package tools;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Regression extends SPL {
   /* **** ATRIBUTE ***** */
-  public Matrix allPoint;
-  private float[] estimationPoint;
+  public Matrix allPoint; // Matrix yang menyimpan semua titik yang dibutuhkan dalam proses regresi
+  private float[] estimationPoint; // array of float yang menyimpan semua titik yang akan diuji
 
   /* **** METHODS ***** */
   /* Konstruktor Matrix */
@@ -52,41 +53,48 @@ public class Regression extends SPL {
     }
   }
 
-  public void readRegressionFile(final String fileName) throws FileNotFoundException {
+  public void readRegressionFile(final String fileName, int n, int m) {
     try {
       /* KAMUS */
-      String directory = "./src/data/" + fileName;
+      String directory = "test/data/" + fileName;
       File file = new File(directory);
-      int Nrow = 0;
-      int Ncol = 0;
-      int i, j; // Indeks
+      int i, j;
 
       Scanner scan = new Scanner(file);
+
       /* ALGORITMA */
-      while (scan.hasNextLine()) {
-        Nrow++;
-        if (Nrow == 1) {
-          while (scan.hasNextFloat()) {
-            Ncol++;
+      Matrix mat = new Matrix(m, n + 1);
+      this.estimationPoint = new float[n];
+
+      scan.nextLine();
+
+      for (i = 0; i < m; i++) {
+        for (j = 0; j < n + 1; j++) {
+          if (scan.hasNextFloat()) {
+            float inputan = scan.nextFloat();
+            mat.setElmt(i, j, inputan);
           }
         }
         scan.nextLine();
       }
-      scan.close();
 
-      Matrix tempMatrix = new Matrix(Nrow, Ncol);
-
-      scan = new Scanner(file);
-      for (i = 0; i < Nrow - 1; i++) {
-        for (j = 0; j < Ncol; j++) {
-          setElmt(i, j, scan.nextFloat());
+      for (j = 0; j < n; j++) {
+        if (scan.hasNextFloat()) {
+          float inputan = scan.nextFloat();
+          this.estimationPoint[j] = inputan;
         }
       }
 
-      this.allPoint = tempMatrix.copyMatrix();
+      this.allPoint = mat.copyMatrix();
+      scan.close();
 
-    } catch (FileNotFoundException e) {
-      System.err.printf("Error: File %s tidak ditemukan", fileName);
+      this.allPoint.displayMatrix();
+      for (i = 0; i < estimationPoint.length; i++) {
+        System.out.println(estimationPoint[i]);
+      }
+      System.out.println();
+    } catch (Exception e) {
+      e.printStackTrace();;
     }
 
   }
@@ -97,8 +105,8 @@ public class Regression extends SPL {
     float[] sigmaAllPointOne;
     float[][] sigmaAllPointTwo;
     /* ALGORITMA */
-    sigmaAllPointOne = new float[this.getColEff() - 1];
-    sigmaAllPointTwo = new float[this.getRowEff() - 1][this.getColEff() - 1];
+    sigmaAllPointOne = new float[this.allPoint.getColEff()];
+    sigmaAllPointTwo = new float[this.allPoint.getColEff() - 1][this.allPoint.getColEff()];
 
     for (i = 0; i < this.allPoint.getColEff(); i++) {
       sigmaAllPointOne[i] = SigmaRegressionOne(i);
@@ -110,8 +118,8 @@ public class Regression extends SPL {
       }
     }
 
-    for (i = 0; i < allPoint.getColEff(); i++) {
-      for (j = 0; j < allPoint.getColEff() + 1; j++) {
+    for (i = 0; i < this.allPoint.getColEff(); i++) {
+      for (j = 0; j < this.allPoint.getColEff() + 1; j++) {
         if (i == 0) {
           if (j == 0) {
             this.setElmt(i, j, allPoint.getRowEff());
@@ -165,4 +173,154 @@ public class Regression extends SPL {
 
     return result;
   }
+
+  public void displayRegression() {
+    int i;
+    int countParametrix = 0;
+    String sentence;
+
+    System.out.println("\nSolusi dari Multiple Linear Regression ini adalah: ");
+    if (this.Status[0] == categorySolution.UNDEFINED) {
+      System.out.println("Permasalahan Regresi tidak memiliki solusi.");
+    } else {
+
+      for (i = 0; i < this.Equation.length; i++) {
+        // JIka solusi adalah parametrix maka hitung jumlahnya
+        if (this.Status[i] == categorySolution.PARAMETRIX) {
+          countParametrix++;
+        }
+        sentence = "B_" + (i) + " = " + this.Equation[i];
+        System.out.println(sentence);
+
+      }
+      if (countParametrix > 0) {
+        char params = 'a';
+        sentence = "\nDengan";
+        for (i = countParametrix; i > 0; i--) {
+          sentence += String.valueOf(params);
+          params++;
+          if (countParametrix > 1) {
+            sentence += ",";
+          }
+        }
+        sentence += "anggota bilangan real";
+        System.out.println(sentence);
+      }
+    }
+
+    System.out.println("Dengan nilai estimasi titik yang diuji adalah " + this.estimate() + "\n");
+  }
+
+  public void writeFileRegression(final String filePath) {
+    int i;
+    int countParametrix = 0;
+    String sentence;
+    FileWriter output = null;
+    try {
+      output = new FileWriter(filePath);
+      output.write("Solusi dari Multiple Linear Regression ini adalah: \n");
+      if (this.Status[0] == categorySolution.UNDEFINED) {
+        output.write("Permasalahan Regresi tidak memiliki solusi.");
+      } else {
+        for (i = 0; i < this.Equation.length; i++) {
+          // Jika solusi adalah parametrix maka hitung jumlahnya
+          if (this.Status[i] == categorySolution.PARAMETRIX) {
+            countParametrix++;
+          }
+          sentence = "B_" + (i) + " = " + this.Equation[i];
+          output.write(sentence);
+          if (i != this.Equation.length - 1) {
+            output.write(System.lineSeparator());
+
+          }
+        }
+      }
+      // Tambahkan kalimat parametrix solution
+      if (countParametrix > 0) {
+        char params = 'a';
+        sentence = "Dengan ";
+        for (i = countParametrix; i > 0; i--) {
+          sentence += String.valueOf(params);
+          params++;
+          if (i > 1) {
+            sentence += ", ";
+          }
+        }
+        sentence += " anggota bilangan real";
+
+        output.write(sentence + "\n");
+      }
+
+      output.write("\nEstimasi nilai yk = " + this.estimate() + "\n");
+      output.close();
+      System.out.println(
+          "Berhasil menyimpan Solusi Regresi pada folder test, file \"" + filePath + "\".");
+    } catch (IOException e) {
+      System.out.println("Something went wrong.");
+      e.printStackTrace();
+    }
+  }
+
+  public void regressionDriver() {
+    int pilihanInput, pilihanOutput;
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Metode Input: ");
+    System.out.println("1. Keyboard (melalui terminal)");
+    System.out.println("2. File");
+    System.out.println("Katakan pilihanmu: ");
+    pilihanInput = scan.nextInt();
+
+    System.out.println("Metode Output: ");
+    System.out.println("1. Terminal");
+    System.out.println("2. File Output");
+    System.out.println("Katakan pilihanmu: ");
+    pilihanOutput = scan.nextInt();
+
+    switch (pilihanInput) {
+      case 1:
+      case 2:
+      default:
+    }
+
+    switch (pilihanOutput) {
+      case 1:
+      case 2:
+      default:
+    }
+
+    scan.close();
+  }
 }
+
+
+// /* CARA MENGGUNAKAN READREGRESSIONKEYBOARD (DRIVER) -> tuliskan di main.java */
+// System.out.print("Masukkan jumlah peubah x : ");
+// int n = globalScan.nextInt();
+// System.out.print("Masukkan banyak sampel : ");
+// int m = globalScan.nextInt();
+
+// Regression regresi = new Regression(n + 1, n + 2);
+// regresi.readRegressionKeyboard(m, n);
+// regresi.compileMatrix();
+// regresi.gaussMethodSPL();
+// System.out.println("Nilai estimasinya adalah " + regresi.estimate());
+
+// /* CARA MENGGUNAKAN READREGRESSIONFILE (DRIVER) -> tuliskan di main.java*/
+// n = 0;
+// m = 0;
+// File file = new File("./test/data/testRegresi.txt");
+// Scanner scan = new Scanner(file);
+// if (scan.hasNextInt()) {
+// n = scan.nextInt();
+// }
+// if (scan.hasNextInt()) {
+// m = scan.nextInt();
+// }
+// Regression reg = new Regression(n + 1, n + 2);
+// reg.readRegressionFile("testRegresi.txt", n, m);
+// reg.gaussMethodSPL();
+// System.out.println("\nSolusi SPL:");
+// reg.displaySPL();
+// System.out.println("Nilai estimasinya adalah " + reg.estimate());
+// scan.close();
